4c9804ab177d2e1818e2e7af255d75b3292b0a27
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-27 09:40:57 +0100

    Done with ex 02

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // 💻 JS Variable – Scope
 
-let myVar = 4;
+// let myVar = 4;
 
-function titi() {
-  const myVar = 5;
-}
+// function titi() {
+//   const myVar = 5;
+// }
 
-titi();
+// titi();
 
 // console.log(myVar);
+
+// 💻 JS Operator – Equals
+
+if (1 === true) {
+    console.log("OK");
+  } else {
+    console.log("NOK");
+  }
+  

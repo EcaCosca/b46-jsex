d18bd4e8655f8e56b80585a5a33acef3171f4854
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-27 09:50:39 +0100

    Done qith ex 3

--- app.js
+++ app.js
@@ -12,9 +12,30 @@
 
 // 💻 JS Operator – Equals
 
-if (1 === true) {
-    console.log("OK");
-  } else {
-    console.log("NOK");
-  }
+// if (1 === true) {
+//     console.log("OK");
+//   } else {
+//     console.log("NOK");
+//   }
+
+// 💻 JS Conditions – If/Else
+
+const compare = (num1, num2) => {
+    let result = ''
+
+    if(num1>num2){
+        result = `The greater number of ${num1} and ${num2} is ${num1}.`
+    }else if(num1<num2){
+        result = 'The greater number of '+num1+' and '+num2+" is "+num2
+    }else{
+        result = 'Both numbers are equal'
+    }
+
+    return result
+}
+
+console.log(compare(5, 6))
+console.log(compare(2, 2))
+console.log(compare(5, 3))
+
   

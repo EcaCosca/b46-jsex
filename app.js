4f8397c21e53fc20d815f629b56ebe8865c97418
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-27 10:02:08 +0100

    Done with ex 4

--- app.js
+++ app.js
@@ -20,22 +20,37 @@
 
 // 💻 JS Conditions – If/Else
 
-const compare = (num1, num2) => {
-    let result = ''
+// const compare = (num1, num2) => {
+//     let result = ''
 
-    if(num1>num2){
-        result = `The greater number of ${num1} and ${num2} is ${num1}.`
-    }else if(num1<num2){
-        result = 'The greater number of '+num1+' and '+num2+" is "+num2
-    }else{
-        result = 'Both numbers are equal'
+//     if(num1>num2){
+//         result = `The greater number of ${num1} and ${num2} is ${num1}.`
+//     }else if(num1<num2){
+//         result = 'The greater number of '+num1+' and '+num2+" is "+num2
+//     }else{
+//         result = 'Both numbers are equal'
+//     }
+
+//     return result
+// }
+
+// console.log(compare(5, 6))
+// console.log(compare(2, 2))
+// console.log(compare(5, 3))
+
+// 💻 JS Conditions – Switch
+
+function myFavouriteColor(color) {
+    switch (color) {
+    case 'blue':
+        return "The color is blue";
+    case 'red':
+        return "The color is red";
+    default:
+        return "It's not a color that I know";
     }
-
-    return result
 }
 
-console.log(compare(5, 6))
-console.log(compare(2, 2))
-console.log(compare(5, 3))
-
-  
+console.log(myFavouriteColor('blue'));
+console.log(myFavouriteColor('red'));
+console.log(myFavouriteColor('green'));
